1964e96ecbbbb23ab0c43d6448275cfa08fad0d2
zacer868 <107080034+zacer868@users.noreply.github.com>
2022-06-07 19:42:57 -0400

    Update settings.py

--- landofart/settings.py
+++ landofart/settings.py
@@ -149,6 +149,8 @@ STATICFILES_DIRS = [
 MEDIA_URL =  'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
+STATIC_ROOT = '/var/www/landofart/static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
